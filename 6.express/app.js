d02cc4258e28113900499b115648621b9348a300
Anshuman Shukla <Anshumanshukla46@gmail.com>
2023-04-06 21:25:39 +0530

    express

--- 6.express/app.js
+++ 6.express/app.js
@@ -3,10 +3,14 @@ const express = require('express')
 // whole site similar to 5.request -> server.js
 
 // express app
-const app = express()
+const app = express();
+
+
+// registering view engine
+app.set('view engine', 'ejs'); // now we could add our template in "views" folder
+
 
 app.listen(3000);
-
 // this exress take care of setheaderfile, status code
 
 // routing
@@ -15,9 +19,7 @@ app.get('/', (req, res) => {
 
 
     // one of absolute path, other if from where we should measure this
-    res.sendFile('./views/index.html',
-        { root: __dirname })
-
+    res.sendFile('./views/index.html', { root: __dirname })
 })
 
 
@@ -37,6 +39,7 @@ app.get('/about-us', (req, res) => {
 // 404 page 
 
 // express read the whole code with url if anything matches and it is on the top send that request only
+
 
 // ALWAYS PUT THIS AT END AS IF IN BETWEEN WE PUT THIS THEN AFTER THIS STATEMENT MOSTLY ALL LINKS WILL NOT "EXECUTED"
 app.use((req, res) => {
